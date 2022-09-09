1409c3f5c0ab808e0c0ab6c10fdb9b563fd970f8
Nabil Salah <nabil.salah203@gmail.com>
2022-09-10 01:11:50 +0200

    adding troops

--- GameFrameWork/Unit.h
+++ GameFrameWork/Unit.h
@@ -1,5 +1,63 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include "Weapon.h"
+using namespace std;
 class Unit
 {
+private:
+	string type;
+	int id;
+	string name;
+	vector<Weapon>weapons;
+	map<string, int>properties;
+public:
+	Unit(int id);
+    string getType()
+    {
+        return type;
+    }
+
+    void setType(string type)
+    {
+        this->type = type;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    void setName(string name)
+    {
+        this->name = name;
+    }
+
+    vector<Weapon> getWeapons()
+    {
+        return weapons;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    void addWeapon(Weapon weapon) {
+        weapons.push_back(weapon);
+    }
+
+    void setProperty(string property, int value)
+    {
+        properties[property]= value;
+    }
+
+    int getProperty(string property) {
+        if (properties.find(property) == properties.end()) {
+            return 0;
+        }
+        return properties[property];
+    }
 };
 
